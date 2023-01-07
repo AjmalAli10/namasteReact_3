7d582df320e08fbb8fd193fc6483e50ffbc52ac6
Ajmal Ali <officialajmalali10@gmail.com>
2023-01-07 20:30:22 +0530

    commit the new and modified files

--- App.js
+++ App.js
@@ -1,5 +1,41 @@
-const heading = React.createElement("h1", {}, "Namaste React sessions 3");
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+// React.createElemet => object => html(DOM).
+
+// const heading = createElement("header", {}, "Namaste React sessions 3",[
+//     createElement("h1", {key:1}, "h1 tag"),
+//     createElement("h2", {key:2}, "h2 tag"),
+//     createElement("h3", {key:3}, "h3 tag")
+// ]);
+
+// SO this form of writing code. make complex structure to debugge and write a large scale code.
+// That's Why JXS is required?
+// JSX is a HTML like syntax but it is not html inside javaScript.
+
+const Heading2 = () =>(
+    <h2>
+    function Component 1
+    </h2>
+);
+const xyz = 10;
+
+// functional Component 
+
+// If I have to use a component inside a component is called "Component Composition"
+
+const HeadingComponent = function (){
+    return (
+        <div>
+        {Heading2()} {/* Component Composition or Composing Component*/}
+        {xyz}
+        <h3>
+            JSX is a HTML like syntax but it is not html inside javaScript.
+        </h3>
+        </div>
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent />);
